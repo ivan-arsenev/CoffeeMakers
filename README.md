9557d05fa75dcb17584f9d0af0c349744d39e616
Ancill <42762155+Ancill@users.noreply.github.com>
2019-10-29 14:35:52 +0300

    some fix

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 In this demo i would like to reproduce kind of social interaction app, where you could register or edit new users.
 Alse it's provides post new messages to news board. Edit or delete this messages.
 
-### Some of usefull commands that I used in through development:
+### Some of useful commands that I used in through development:
 
     npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
     npm i -D nodemon concurrently
     npm run server
+
+### Some of usefull links to docs:
+
+    https://express-validator.github.io/docs/
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
-
+const { check, validationResult } = require('express-validator');
 // @route POST api/users
 // @desc Register route
 // @access Public
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
 
-  res.send('User route');
-});
+    res.send('User route');
+  }
+);
 
 module.exports = router;
