0273f00cd6df22a74c9bb1affc8a06ec35b191d6
Ancill <42762155+Ancill@users.noreply.github.com>
2019-10-30 18:07:46 +0300

    added post shema and initialazed new  reqest

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route GET api/post
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('Post route'));
+// @route   POST api/post
+// @desc    Create a post
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required!')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => res.send('Post route')
+);
 
 module.exports = router;
